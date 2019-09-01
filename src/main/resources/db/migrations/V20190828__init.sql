610102065fade5932126929ecb1e87e85195926c
jerrylewis <jerrery520@gmail.com>
2019-09-01 22:11:39 +0800

    [jerry] update the data model

--- src/main/resources/db/migrations/V20190828__init.sql
+++ src/main/resources/db/migrations/V20190828__init.sql
@@ -6,10 +6,10 @@ CREATE TABLE `users`
     `email`            varchar(255) NOT NULL DEFAULT '' COMMENT '电子邮箱',
     `encrypt_password` varchar(255) NOT NULL DEFAULT '' COMMENT '加密密码',
     `salt`             varchar(255) NOT NULL DEFAULT '' COMMENT '密码串',
-    `user_type`        smallint(6)  NOT NULL DEFAULT '0' COMMENT '用户类型：0 普通用户 1 设计师',
+    `user_type`        varchar(255) NOT NULL DEFAULT 'user' COMMENT '用户类型：user 普通用户 designer 设计师 admin',
     `nickname`         varchar(36)           DEFAULT '' COMMENT '用户昵称',
     `avatar`           varchar(255)          DEFAULT '' COMMENT '用户头像',
-    `status`           smallint(6)  NOT NULL DEFAULT '0' COMMENT '用户状态：0 活动的 1 禁用',
+    `status`           varchar(255) NOT NULL DEFAULT 'active' COMMENT '用户状态：active 活动的 ban 禁用',
     `last_login_at`    datetime              DEFAULT NULL COMMENT '最后登陆时间',
     `last_login_ip`    varchar(39)  NOT NULL DEFAULT '' COMMENT '最后登陆IP',
     `registration_at`  datetime              DEFAULT NULL COMMENT '注册时间',
@@ -30,8 +30,8 @@ CREATE TABLE `categories`
     `id`         char(36)     NOT NULL DEFAULT '' COMMENT '主键uuid',
     `name`       varchar(255) NOT NULL DEFAULT '' COMMENT '英文分类名: haibao、dm etc.',
     `name_chn`   varchar(255) NOT NULL DEFAULT '' COMMENT '中文分类名：手机海报、DM宣传单 etc.',
-    `type`       smallint(6)  NOT NULL DEFAULT '0' COMMENT '分类：0 分类：电商， 1: group 分组：海报',
-    `status`     smallint(6)  NOT NULL DEFAULT '0' COMMENT '分类状态：0 发布的 1 下架的',
+    `type`       varchar(255) NOT NULL DEFAULT 'category' COMMENT '分类：category 分类：电商 group 分组：海报',
+    `status`     varchar(255) NOT NULL DEFAULT 'unpublished' COMMENT '分类状态：published 发布的 unpublished 下架的',
     `created_at` datetime     NOT NULL COMMENT '创建时间',
     `updated_at` datetime     NOT NULL COMMENT '更新时间',
     PRIMARY KEY (`id`)
@@ -84,8 +84,10 @@ CREATE TABLE `templates`
 
     `tag_meta`           text COMMENT '模板标签：json：{"level1":"发布会,新品,新品首发","level3":"蓝色,黑色,白色","level2":"品牌,水彩,笔触,纹理,简约,商务,媒体,记者,新产品,请帖,请柬"}',
 
+    `verified`           varchar(255) NOT NULL DEFAULT 'unaudited' COMMENT '发布状态：unaudited 未审核  rejected 驳回 checked 已审核',
+    `verified_at`        datetime COMMENT '审核通过时间',
+    `publish_status`     varchar(255) NOT NULL DEFAULT 'unpublished' COMMENT '发布状态：published 已发布 unpublished 未发布',
     `published_at`       datetime COMMENT '发布时间',
-    `publish_status`     smallint(6)  NOT NULL DEFAULT '0' COMMENT '发布状态：0 未发布 1 已发布',
 
     `used_count`         int(11)      NOT NULL DEFAULT 0 COMMENT '使用次数',
     `created_at`         datetime     NOT NULL COMMENT '创建时间',
@@ -100,15 +102,17 @@ CREATE TABLE `assets`
     `user_id`        char(36)     NOT NULL DEFAULT '' COMMENT '用户ID',
     `name`           varchar(255) NOT NULL DEFAULT '' COMMENT '素材名',
     `name_chn`       varchar(255) NOT NULL DEFAULT '' COMMENT '素材名：中文',
-    `type`           smallint(6)  NOT NULL DEFAULT '0' COMMENT '类型：0 背景图，1 字体, 2，贴纸',
+    `type`           varchar(255) NOT NULL DEFAULT 'color' COMMENT '类型：color 颜色 background 背景图， font 字体, sticker，贴纸',
 
     `resource_url`   varchar(255) NOT NULL DEFAULT '' COMMENT '图片、资源地址',
     `thumbnail_url`  varchar(255) NOT NULL DEFAULT '' COMMENT '缩略图地址',
     `resources_meta` longtext COMMENT '资源:json {}',
 
     `personal`       smallint(6)  NOT NULL DEFAULT '0' COMMENT '发布状态：0 私人的 1 官网的',
+    `verified`       varchar(255) NOT NULL DEFAULT 'unaudited' COMMENT '发布状态：unaudited 未审核  rejected 驳回 checked 已审核',
     `verified_at`    datetime COMMENT '审核通过时间',
-    `verified`       smallint(6)  NOT NULL DEFAULT '0' COMMENT '发布状态：0 未审核 1 已审核',
+    `publish_status` varchar(255) NOT NULL DEFAULT 'unpublished' COMMENT '发布状态：published 已发布 unpublished 未发布',
+    `published_at`   datetime COMMENT '发布时间',
 
     `created_at`     datetime     NOT NULL COMMENT '创建时间',
     `updated_at`     datetime     NOT NULL COMMENT '更新时间',
