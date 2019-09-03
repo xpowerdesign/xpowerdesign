1417d4d52a9aa5bf3ad4e10b6b67480dbed0e477
jerrylewis <jerrery520@gmail.com>
2019-09-03 17:15:21 +0800

    [jerry] update the sql for category

--- src/main/resources/db/migrations/V20190828__init.sql
+++ src/main/resources/db/migrations/V20190828__init.sql
@@ -27,13 +27,23 @@ CREATE TABLE `users`
 
 CREATE TABLE `categories`
 (
-    `id`         char(36)     NOT NULL DEFAULT '' COMMENT '主键uuid',
-    `name`       varchar(255) NOT NULL DEFAULT '' COMMENT '英文分类名: haibao、dm etc.',
-    `name_chn`   varchar(255) NOT NULL DEFAULT '' COMMENT '中文分类名：手机海报、DM宣传单 etc.',
-    `type`       varchar(255) NOT NULL DEFAULT 'category' COMMENT '分类：category 分类：电商 group 分组：海报',
-    `status`     varchar(255) NOT NULL DEFAULT 'unpublished' COMMENT '分类状态：published 发布的 unpublished 下架的',
-    `created_at` datetime     NOT NULL COMMENT '创建时间',
-    `updated_at` datetime     NOT NULL COMMENT '更新时间',
+    `id`            char(36)     NOT NULL DEFAULT '' COMMENT '主键uuid',
+    `name`          varchar(255) NOT NULL DEFAULT '' COMMENT '英文分类名: haibao、dm etc.',
+    `name_chn`      varchar(255) NOT NULL DEFAULT '' COMMENT '中文分类名：手机海报、DM宣传单 etc.',
+    `type`          varchar(255) NOT NULL DEFAULT 'category' COMMENT '分类：category 分类：电商 group 分组：海报',
+    `width`         int(11)      NOT NULL DEFAULT 0 COMMENT '宽度 px',
+    `height`        int(11)      NOT NULL DEFAULT 0 COMMENT '高度 px',
+    `dpi`           int(11)      NOT NULL DEFAULT 0 COMMENT '分辨率',
+    `doc_width`     int(11)      NOT NULL DEFAULT 0 COMMENT '文档宽度',
+    `doc_height`    int(11)      NOT NULL DEFAULT 0 COMMENT '文档高度',
+    `doc_unit`      varchar(10)  NOT NULL DEFAULT '' COMMENT '文档单位: px, cm, etc.',
+    `size`          varchar(255) NOT NULL DEFAULT '' COMMENT '尺寸',
+    `cover`         varchar(255) NOT NULL DEFAULT '' COMMENT '封面',
+    `cover_display` varchar(255) NOT NULL DEFAULT '' COMMENT '封面背部或者展示页',
+    `notices`       text COMMENT '提示',
+    `status`        varchar(255) NOT NULL DEFAULT 'unpublished' COMMENT '分类状态：published 发布的 unpublished 下架的',
+    `created_at`    datetime     NOT NULL COMMENT '创建时间',
+    `updated_at`    datetime     NOT NULL COMMENT '更新时间',
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4 COMMENT '类别表';
@@ -47,9 +57,14 @@ CREATE TABLE `projects`
     `user_id`           char(36)     NOT NULL DEFAULT '' COMMENT '用户ID',
     `title`             varchar(255) NOT NULL DEFAULT '' COMMENT '工程标题',
     `description`       varchar(255) NOT NULL DEFAULT '' COMMENT '工程描述',
-    `width`             int(11)      NOT NULL DEFAULT 0 COMMENT '工程宽度',
-    `height`            int(11)      NOT NULL DEFAULT 0 COMMENT '工程高度',
-    `unit`              varchar(10)  NOT NULL DEFAULT '' COMMENT '工程单位: px, cm, etc.',
+
+    `width`             int(11)      NOT NULL DEFAULT 0 COMMENT '宽度 px',
+    `height`            int(11)      NOT NULL DEFAULT 0 COMMENT '高度 px',
+    `dpi`               int(11)      NOT NULL DEFAULT 0 COMMENT '分辨率',
+    `doc_width`         int(11)      NOT NULL DEFAULT 0 COMMENT '文档宽度',
+    `doc_height`        int(11)      NOT NULL DEFAULT 0 COMMENT '文档高度',
+    `doc_unit`          varchar(10)  NOT NULL DEFAULT '' COMMENT '文档单位: px, cm, etc.',
+
     `image_url`         varchar(255) NOT NULL DEFAULT '' COMMENT '图片地址',
     `json_url`          varchar(255) NOT NULL DEFAULT '' COMMENT 'json地址：存储工程元数据',
     `svg_url`           varchar(255) NOT NULL DEFAULT '' COMMENT 'svg地址',
@@ -74,9 +89,14 @@ CREATE TABLE `templates`
     `name`               varchar(255) NOT NULL DEFAULT '' COMMENT '模板名称',
     `title`              varchar(255) NOT NULL DEFAULT '' COMMENT '模板标题',
     `description`        varchar(255) NOT NULL DEFAULT '' COMMENT '工程描述',
-    `width`              int(11)      NOT NULL DEFAULT 0 COMMENT '工程宽度',
-    `height`             int(11)      NOT NULL DEFAULT 0 COMMENT '工程高度',
-    `unit`               varchar(10)  NOT NULL DEFAULT '' COMMENT '工程单位: px, cm, etc.',
+
+    `width`              int(11)      NOT NULL DEFAULT 0 COMMENT '宽度 px',
+    `height`             int(11)      NOT NULL DEFAULT 0 COMMENT '高度 px',
+    `dpi`                int(11)      NOT NULL DEFAULT 0 COMMENT '分辨率',
+    `doc_width`          int(11)      NOT NULL DEFAULT 0 COMMENT '文档宽度',
+    `doc_height`         int(11)      NOT NULL DEFAULT 0 COMMENT '文档高度',
+    `doc_unit`           varchar(10)  NOT NULL DEFAULT '' COMMENT '文档单位: px, cm, etc.',
+
     `json_url`           varchar(255) NOT NULL DEFAULT '' COMMENT 'json地址：存储工程元数据',
     `svg_url`            varchar(255) NOT NULL DEFAULT '' COMMENT 'svg地址',
     `image_url`          varchar(255) NOT NULL DEFAULT '' COMMENT '图片地址',
